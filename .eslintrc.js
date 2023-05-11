d53969558bc371836adf1cc709a6731969f8144a
Ansh Goyal <anshgoyal1704@gmail.com>
2023-05-11 10:42:45 +0530

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'plugin:sonarjs/recommended',
+      'plugin:security/recommended',
+      'plugin:react/recommended',
+      'plugin:jest/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'sonarjs', 'security', 'react', 'jest'],
+    rules: {
+      // Additional rules go here
+    },
+  };
+  

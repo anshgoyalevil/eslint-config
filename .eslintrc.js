a1e784a2c6bf48d0d0f878d871302f2b43fff4f4
Ansh Goyal <anshgoyal1704@gmail.com>
2023-11-03 16:28:16 +0530

    update plugin
    Signed-off-by: Ansh Goyal <anshgoyal1704@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,29 @@
 module.exports = {
-    extends: [
-      'plugin:@typescript-eslint/recommended',
-      'plugin:sonarjs/recommended',
-      'plugin:security/recommended',
-      'plugin:react/recommended',
-      'plugin:jest/recommended',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint', 'sonarjs', 'security', 'react', 'jest'],
-    rules: {
-      // Additional rules go here
-      // Created with love by Ansh Goyal
-    },
-  };
-  
+  "env": {
+    "browser": true,
+    "es6": true,
+  },
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly",
+    "React": "writable"
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:security/recommended',
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
+    'plugin:cypress/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'sonarjs', 'security', 'react', 'jest'],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "no-useless-catch": "off",
+  },
+};
